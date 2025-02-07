6a2461e407cb48f77306fe9cc03fb38afa096c71
alphy2023 <smarthr406@gmail.com>
2025-02-07 11:15:42 +0300

    user removed from sockets

--- src/store/socketStore.ts
+++ src/store/socketStore.ts
@@ -19,18 +19,14 @@ export type User = {
 
 interface SocketState {
   socket: Socket | null;
-  user: User | null;
   onlineUsers: OnlineUser[];
-  setUser: (user: User | null) => void;
   setSocket: (socket: Socket | null) => void;
   setOnlineUsers: (users: OnlineUser[]) => void;
 }
 
 export const useSocketStore = create<SocketState>((set) => ({
   socket: null,
-  user: null,
   onlineUsers: [],
-  setUser: (user) => set({ user }),
   setSocket: (socket) => set({ socket }),
   setOnlineUsers: (users) => set({ onlineUsers: users }),
 }));
