e3dde2102492d5527409d561f81c92cd59bf6c56
alphy2023 <smarthr406@gmail.com>
2025-02-07 11:10:42 +0300

    user socket issue

--- src/store/socketStore.ts
+++ src/store/socketStore.ts
@@ -1,37 +1,36 @@
 "use client";
 
 import { create } from "zustand";
-import { Socket } from "socket.io-client";
+import type { Socket } from "socket.io-client";
 
-export type OnlineUser ={
-    socketId?:string;
-    name?:string;
-    location?:string;
-    image?:string;
+export type OnlineUser = {
+  socketId?: string;
+  name?: string;
+  location?: string;
+  image?: string;
+};
 
-}
-export type User={
-    userId?:string;
-    name?:string;
-    email?:string;
-    phone?:string;
-
-}
+export type User = {
+  userId?: string;
+  name?: string;
+  email?: string;
+  phone?: string;
+};
 
 interface SocketState {
   socket: Socket | null;
-  user:User|null;
-  setUser:(user:User | null)=>void;
+  user: User | null;
   onlineUsers: OnlineUser[];
+  setUser: (user: User | null) => void;
   setSocket: (socket: Socket | null) => void;
   setOnlineUsers: (users: OnlineUser[]) => void;
 }
 
 export const useSocketStore = create<SocketState>((set) => ({
   socket: null,
-  user:null,
-  setUser:(user)=>set({user:user}),
+  user: null,
   onlineUsers: [],
+  setUser: (user) => set({ user }),
   setSocket: (socket) => set({ socket }),
   setOnlineUsers: (users) => set({ onlineUsers: users }),
 }));
